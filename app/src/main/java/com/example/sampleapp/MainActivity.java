ff1b4e41f8b63efefb63d94669bf05e5ce04477e
INVICTUS9421 <126803552+INVICTUS9421@users.noreply.github.com>
2024-08-11 14:13:27 +0530

    login page moves to registerpage

--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.sampleapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginpage);
+
+        TextView registerTextView = findViewById(R.id.registerTextView);
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, registerpage.class);
+                startActivity(intent);
+            }
+        });
 
         password = findViewById(R.id.password);
         button = findViewById(R.id.loginbutton);
---
+++ app/src/main/java/com/example/sampleapp/registerpage.java
@@ -0,0 +1,13 @@
+package com.example.sampleapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class registerpage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registerpage);  // Refer to registerpage.xml
+    }
+}
