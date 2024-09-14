ff829517cffebc68fa4b3097be093840b57c4757
INVICTUS9421 <126803552+INVICTUS9421@users.noreply.github.com>
2024-09-14 11:05:54 +0530

    worked on doctors

--- app/src/main/java/com/example/sampleapp/registerpage.java
+++ app/src/main/java/com/example/sampleapp/registerpage.java
@@ -53,10 +53,52 @@ public class registerpage extends AppCompatActivity {
                 }
                 else
                 {
-                    Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_SHORT).show();
+                    if (pass.compareTo(cpass)==0)
+                    {
+                        if (isValid(pass))
+                        {
+                            Toast.makeText(getApplicationContext(), "Record Inserted", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(registerpage.this,loginpage.class));
+                        }
+                        else
+                        {
+                            Toast.makeText(getApplicationContext(), "Password must contain atleast 8 characters,having letter digit ans special characters", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "Password and confirm password do not match", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
-
+    }
+    public static boolean isValid(String password)
+    {
+        int case1=0,case2=0,case3=0;
+        if(password.length()<8)
+        {
+            return false;
+        }
+        else
+        {
+            for (int i=0;i<password.length();i++)
+            {
+                if (Character.isDigit(password.charAt(i)))
+                    case1 = 1;
+            }
+            for (int i=0;i<password.length();i++)
+            {
+                if (Character.isAlphabetic(password.charAt(i)))
+                    case2 = 1;
+            }
+            for (int i=0;i<password.length();i++)
+            {
+                char c = password.charAt(i);
+                if (c>=33 && c<=46 || c==64)
+                    case3=1;
+            }
+            return case1 == 1 && case2 == 1 && case3 == 1;
+        }
     }
 }
